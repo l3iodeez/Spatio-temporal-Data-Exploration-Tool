9403151d8a06e8d6d4fb51b6b9e4f212111342bb
Henry Dotson <henry.dotson@food52.com>
2018-11-13 20:13:43 -0500

    Add caching.

--- app/serializers/site_serializer.rb
+++ app/serializers/site_serializer.rb
@@ -3,8 +3,10 @@
 class SiteSerializer < ActiveModel::Serializer
   attributes :id, :measurements
   def measurements
-    object.measurements.map do |m|
-      MeasurementSerializer.new(m, adapter: :json)
+    Rails.cache.fetch("site-#{object.id}-measurements", :expires_in => 30.days) do
+      object.measurements.map do |m|
+        MeasurementSerializer.new(m, adapter: :json)
+      end
     end
   end
 end
