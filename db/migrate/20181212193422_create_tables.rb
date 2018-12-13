9f1f6c1bacb6ff3604f4b0c0d1cc1f304710974b
Henry Dotson <l3iodeez@gmail.com>
2018-12-13 15:35:34 -0500

    Update title.

--- db/migrate/20181212193422_create_tables.rb
+++ db/migrate/20181212193422_create_tables.rb
@@ -8,7 +8,6 @@ class CreateTables < ActiveRecord::Migration
       t.string  "units"
       t.string  "measure_type"
       t.string  "data_provider"
-      t.json    "full_json"
     end
 
     create_table "saved_selections", force: :cascade do |t|
@@ -20,18 +19,34 @@ class CreateTables < ActiveRecord::Migration
     add_index "saved_selections", ["user_id"], name: "index_saved_selections_on_user_id", using: :btree
 
     create_table "sites", force: :cascade do |t|
+      t.string   "agency"
+      t.bigint  "site_number", :unique => true
       t.string   "site_name"
-      t.string   "site_reference"
+      t.string   "site_type"
       t.float    "latitude"
       t.float    "longitude"
-      t.datetime "start"
-      t.datetime "end"
-      t.json     "full_json"
+      t.string    "coordinate_accuracy"
+      t.string    "coordinate_datum"
+      t.string    "altitude"
+      t.string    "altitude_accuracy"
+      t.string    "altitude_datum"
+      t.string    "hydrologic_unit_code"
+      t.string    "data_type"
+      t.string    "parameter_code"
+      t.string    "statistical_code"
+      t.string    "timeseries_id"
+      t.string    "measurement_description"
+      t.string    "medium_group_code"
+      t.string    "parameter_group_code"
+      t.string    "srs_id"
+      t.string    "access_code"
+      t.datetime "start_date"
+      t.datetime "end_date"
       t.integer  "measure_count"
+      t.string   "address"
       t.string   "city"
       t.string   "state"
       t.string   "zip"
-      t.string   "address"
     end
 
     create_table "users", force: :cascade do |t|
--- db/schema.rb
+++ db/schema.rb
@@ -36,18 +36,34 @@ ActiveRecord::Schema.define(version: 20181212193422) do
   add_index "saved_selections", ["user_id"], name: "index_saved_selections_on_user_id", using: :btree
 
   create_table "sites", force: :cascade do |t|
+    t.string   "agency"
+    t.integer  "site_number",             limit: 8
     t.string   "site_name"
-    t.string   "site_reference"
+    t.string   "site_type"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "start"
-    t.datetime "end"
-    t.json     "full_json"
+    t.string   "coordinate_accuracy"
+    t.string   "coordinate_datum"
+    t.string   "altitude"
+    t.string   "altitude_accuracy"
+    t.string   "altitude_datum"
+    t.string   "hydrologic_unit_code"
+    t.string   "data_type"
+    t.string   "parameter_code"
+    t.string   "statistical_code"
+    t.string   "timeseries_id"
+    t.string   "measurement_description"
+    t.string   "medium_group_code"
+    t.string   "parameter_group_code"
+    t.string   "srs_id"
+    t.string   "access_code"
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.integer  "measure_count"
+    t.string   "address"
     t.string   "city"
     t.string   "state"
     t.string   "zip"
-    t.string   "address"
   end
 
   create_table "users", force: :cascade do |t|
